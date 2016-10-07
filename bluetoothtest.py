364a4907804f3c651d75ec494bbafba8f18ab767
kyubinhan11 <kyubinhan11@naver.com>
2016-10-07 21:16:39 +0000

    bluetoothtest.py edited

--- bluetoothtest.py
+++ bluetoothtest.py
@@ -1,40 +1,55 @@
 import os
+import picamera
+import sys
+import io
+import select
 import glob
 import time
 import RPi.GPIO as GPIO
+import base64
+import requests
+import os
+import json
+import argparse
 from bluetooth import *
 
+counter = 0
+
+def get_encode(image):
+    """
+    1. encode binary data(image) to printable ASCII characeters
+    2. send the encoded data to the server using REST POST method
+    3. get the result from the server and print out the label
+    """
+
+    with open(image, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+
+    return encoded_string
+
+def take_picture():
+        photo_file = "image" + str(counter) + ".jpg"
+        with picamera.PiCamera() as camera:
+                camera.capture(photo_file)
+                print(photo_file + " is just captured!")
+	counter += 1
+        return photo_file
+
+def is_letter_input(letter):
+        # Utility function to check if a specific character is available on stdin.
+        # Comparison is case insensitive.
+        if select.select([sys.stdin,],[],[],0.0)[0]:
+                input_char = sys.stdin.read(1)
+                return input_char.lower() == letter.lower()
+        return False
+   
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
-"""
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c
-"""
-#while True:
-#	print(read_temp())	
-#	time.sleep(1)
 
 
 server_sock=BluetoothSocket( RFCOMM )
@@ -58,26 +73,30 @@ while True:
 	print "Accepted connection from ", client_info
 
 	try:
-        data = client_sock.recv(1024)
-    	if len(data) == 0: break
-        print "received [%s]" % data
+        	data = client_sock.recv(1024)
+    		if len(data) == 0: break
+       	        print "received [%s]" % data
 
 		if data == 'takePicture':
 			#take a picture function here!
-			#data = str(read_temp())+'!'
-			data = "hello world!"
-		elif data == 'lightOn':
+			"""
+			photo_file = take_picture()
+			data = str(get_encode(photo_file)) + "!"
+			"""
+ 
+			data = str(get_encode("greentext.jpg")) + "!"	
+		elif data == 'volumeUp':
 			GPIO.output(17,False)
-			data = 'light on!'
+			data = 'Volume up!'
 		elif data == 'lightOff':
 			GPIO.output(17,True)
 			data = 'light off!'
 		else:
 			data = 'WTF!' 
 
-	    client_sock.send(data)
+	        client_sock.send(data)
 		print "sending [%s]" % data
-
+		print "number of letters" + str(len(data))
 	except IOError:
 		pass
 
