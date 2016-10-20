e343bf28541cbd510bafd49bce44427481e24987
kyubinhan11 <kyubinhan11@naver.com>
2016-10-20 13:39:27 -0700

    Update espeak.py

--- espeak.py
+++ espeak.py
@@ -1,4 +1,53 @@
 import subprocess
+import collections
 
-def speak(string):
-	subprocess.call('espeak -v+f3 \"'+ string +'\"', shell = True)
+class ESpeak:
+    def __init__(self, amplitude=100, word_gap=10, capitals=1, line_length=1,
+                 pitch=50, speed=175, voice='en', spell_punctuation=[],
+                 split=''):
+        """
+        The base ESpeak class. You can set espeak's arguments from the
+        named parameters or change them after creation using the properties
+        """
+
+        args = [('amplitude',         ['-a', amplitude, int]),
+                ('word_gap',          ['-g', word_gap, int]),
+                ('capitals',          ['-k', capitals, int]),
+                ('line_length',       ['-l', line_length, int]),
+                ('pitch',             ['-p', pitch, int]),
+                ('speed',             ['-s', speed, int]),
+                ('voice',             ['-v', voice, str]),
+                ('spell_punctuation', ['--punct=', ''.join(spell_punctuation),
+                                       str]),
+                ('split',             ['--split=', split, str])]
+        self.args = collections.OrderedDict(args)
+	
+	
+    def _espeak_args(self, text, filename=''):
+        self._validate_args()
+        if filename:
+            save = ["{0}{1}".format(self.split[0], self.split[1]),
+                    "-w{}".format(filename)]
+        else:
+            save = []
+        args = ['espeak'] + \
+               ["{0}{1}".format(v[0], v[1]) for v in self.args.values()] + \
+               ['-x'] + save + [text]
+        return args
+
+    def _execute(self, cmd):
+	subprocess.call(cmd)
+	
+    def say(self, text):
+        """
+        Make espeak read the text directly
+        Parameters
+        ----------
+        text : str
+            The text to be read
+        Returns
+        -------
+        phoneme : str
+            The phoneme, as read by speak
+        """
+        return self._execute(self._espeak_args(text))
