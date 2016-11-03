33d776139a4a5e354ec7677a965dbfaba1e47e97
kyubinhan11 <kyubinhan11@naver.com>
2016-11-02 23:17:50 -0700

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -22,9 +22,10 @@ RESTful API Service  available method : GET, POST
   POST : Get encoded binary data(image) and decode such the encoding back to binary data
          Regenerate the image and save it then open the file as OpenCV picture
          Start Image cropping and send the cropped picture to Google Vision API
-         Get the text from API and generate a json file that contains the text
-         Return the text in json type
-
+         Get the label from API and generate a json file that contains this label
+         Send it back to client(Pi) in json type
+   
+   HOW TO USE
    ex. url = "http://localhost:5000/todo/api/v1.0/images"
        data = {'encode' : encoded_string }
        headers = {'Content-Type': 'application/json'}
@@ -59,12 +60,13 @@ def get_image_and_produce_label():
     """
     reconpile the received picture and save it then
     openCV will start detecting the color and start cropping
-    if there is a face in the picture --> return emotion
-        else 
-            if there is red color detected  --> return label
-            if there is green color detected --> return text
+    
+    if there is a face in the picture --> return emotion        
+    elif there is some text in the picture  --> return text
+    	and if there is a green contour --> crop the picture then return text
+    else  --> crop the picture return label
+    
     """
-
     # the type of the data should be json and have to have key 'encode'
     # otherwise I will just abort with 400    
     if not request.json or not 'encode' in request.json:
@@ -86,17 +88,18 @@ def get_image_and_produce_label():
 
     # detect color and crop the picture then 
     # return 0, 1, 2 depending on the color 
-    which_color = recognition.color_detect_and_crop_image(photo_file)
+    
 
     if(google.is_face() == True):
-        text = "" + google.get_face()    
+        text = "Face Found" + google.get_face()    
 
     elif(google.is_text() == True): # 1: green      
-        text = "" + google.get_text()	
+        text = "Text Found" + google.get_text()
+	which_color = recognition.color_detect_and_crop_image(photo_file)
 	if(which_color == 1):
 		text = "" + recognition.get_text(photo_file)	
     else:
-        text = "" + recognition.get_label(photo_file)
+        text = "Label Found" + recognition.get_label(photo_file)
 
     
     #cv2.imshow('yes', imcv)
